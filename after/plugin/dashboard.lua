e833e996409d939b76df719705e9a6dd0cc6be40
Joris Hartog <jorishartog@hotmail.com>
2022-12-27 21:13:30 +0100

    Update splash screen

--- after/plugin/dashboard.lua
+++ after/plugin/dashboard.lua
@@ -1,15 +1,21 @@
 local db = require('dashboard')
+local home = os.getenv('HOME')
+
+db.preview_command = 'cat | lolcat'
+db.preview_file_path = home .. '/.config/nvim/static/island.cat'
+db.preview_file_height = 8
+db.preview_file_width = 9
 db.custom_center = {
     {icon = '  ',
-    desc ='File Browser                            ',
+    desc = 'File Browser  ',
     action =  'Ex',
-    shortcut = 'SPC p v'},
+    shortcut = ';pv'},
     {icon = '  ',
-    desc = 'Find  File                              ',
+    desc = 'Find File     ',
     action = 'Telescope find_files find_command=rg,--hidden,--files',
-    shortcut = 'SPC p f'},
+    shortcut = ';pf'},
     {icon = '  ',
-    desc = 'Find  word                              ',
+    desc = 'Find word     ',
     action = 'Telescope live_grep',
-    shortcut = 'SPC p s'},
+    shortcut = ';ps'},
 }
