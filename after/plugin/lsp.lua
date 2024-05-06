75328d87559a836cf3faf8c6edb6f09a35cb656f
Joris Hartog <jorishartog@hotmail.com>
2024-05-06 13:06:56 +0200

    Fix autocomplete for leptos

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,3 +19,19 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true,
 })
+
+-- Fix autocompletion within leptos #[server] and #[component]
+require('lspconfig').rust_analyzer.setup {
+  settings = {
+    ["rust-analyzer"] = {
+      procMacro = {
+        ignored = {
+            leptos_macro = {
+                "component",
+                "server",
+            },
+        },
+      },
+    },
+  }
+}
