5de1acb16ba471db8456943344407a2fbceb859e
Joris Hartog <jorishartog@hotmail.com>
2024-05-06 13:06:29 +0200

    Remove codeium

--- lua/jhartog/packer.lua
+++ lua/jhartog/packer.lua
@@ -79,15 +79,4 @@ return packer.startup(function(use)
 
     -- 🦘 leap - A motion plugin
     use('ggandor/leap.nvim')
-
-    -- 🤖 codeium - A code completion AI
-    use({
-      'Exafunction/codeium.vim',
-      config = function ()
-        vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-        vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-        vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-        vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-      end
-    })
 end)
